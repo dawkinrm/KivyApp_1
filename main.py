c5746f8ff4f1973e0d062dbdfd024cdc7b07b719
dawkinrm <dawkinrm@dukes.jmu.edu>
2015-02-05 14:53:25 -0800

    second commit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from kivy.app import App
 from kivy.core.audio import SoundLoader
 
 class HelloWorldApp(App):
-  def play_music(self):
-    self.sound = SoundLoader.load('Jesse_Cook-Dance_Of_Spring.ogg')
-    self.sound.play()
+    def play_music(self):
+      self.sound = SoundLoader.load('Jesse_Cook-Dance_Of_Spring.ogg')
+      self.sound.play()
 
 if __name__ == "__main__":
-  HelloWorldApp().run()
+    HelloWorldApp().run()
